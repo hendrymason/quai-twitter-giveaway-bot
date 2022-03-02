502b4708e84063a9d31d671535293dd5718cd2a1
mheez <68663997+hendrymason@users.noreply.github.com>
2022-03-02 00:19:33 -0600

    fully updated

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from xxlimited import new
+from numpy import iterable
 import tweepy
 import csv
 from typing import Iterable
+import datetime
+from datetime import timedelta
 
 # Giveaway Rules:
 # 1. Following @Quainetwork and Alanorwick -> check following
@@ -14,11 +18,12 @@ quai_id = 1306071657174441985
 alans_id = 516947335
 giveaway_thread = [1496183164917665793, 1496183164917665793]
 quai_username = 'quainetwork'
-third_bearer = "AAAAAAAAAAAAAAAAAAAAAJlpZQEAAAAAjwv7Gg01zTG7ck30cyqxiwQcm9U%3DPNHl3UjpTpKdAjtVcrOs8tZqprtwO6TUKl61XWrXzKKVwgkMDA" #coles
-fourth_bearer = "AAAAAAAAAAAAAAAAAAAAAJxpZQEAAAAAexanqSH%2B10AH6FDOHTOqAlqiI8g%3D9hHfSUY0qNXjDGBwlBRNqhXelDcDYpNvnW4VGtpiroOAlabxaR" #owens
+third_bearer = "" #coles
+fourth_bearer = "" #owens
+fifth_bearer = "" #maxs
 
 # initialize client
-client = tweepy.Client(bearer_token=third_bearer)
+client = tweepy.Client(bearer_token=fourth_bearer)
 
 # 1. check if following @quainetwork and @alanorwick
 alans_followers_resp = client.get_users_followers(id=alans_id, max_results=1000)
@@ -52,31 +57,39 @@ collect_users_followers(quai_id)
 # create a list of users who follow BOTH alan and quai
 following_accounts = [username for username in quais_followers if username in alans_followers]
 
-print("---- Total Accounts following Quai and Alan ----")
-print(len(following_accounts))
-
-#def paginate(response, _method, _id, _field _field_value):
-def paginate(response, _id, type):
-    pagination = True
-    collected_data = []
-    while pagination == True:
-        next_token = response.meta['next_token']
-        if type == "likes":
-            new_response = client.get_liking_users(id=_id)
-        elif type == "retweets":
-            new_response = client.get_retweeters(id=_id)
-        collected_data.append(new_response.data)
-        try:
-            if new_response.meta["next_token"]:
-                new_token = new_response.meta["next_token"]
-        except:
-            print("done paginating")
-            pagination = False
-    return collected_data
-
+#print("---- Total Accounts following Quai and Alan ----")
+#print(len(following_accounts))
 
 # 2. Check for users who liked+retweted the tweets in giveaway thread
 # pull likers and retweeters list
+def paginate(response, _id, user_type):
+    pagination = True
+    collected_data = []
+    next_token = response.meta['next_token']
+    pagination_count = 0
+    while pagination == True:
+        if user_type == "likes":
+            new_response = client.get_liking_users(id=_id, pagination_token=next_token)
+        elif user_type == "retweets":
+            new_response = client.get_retweeters(id=_id, pagination_token=next_token)
+        
+        if new_response.data != None:
+            for tweet_data in new_response.data:
+                collected_data.append(tweet_data)
+            pagination_count += 1
+        else:
+            print("pulled a 'None' Type for response")
+        
+        try:
+            if new_response.meta["next_token"]:
+                next_token = new_response.meta["next_token"]
+        except:
+            print("done paginating")
+            print("paginated " + user_type + " " + str(pagination_count) + " times.")
+            pagination = False
+    
+    return collected_data
+
 for tweet_id in giveaway_thread:
     likers_list = []
     retweeters_list = []
@@ -90,14 +103,16 @@ for tweet_id in giveaway_thread:
     try:
         if likers_response.meta['next_token']:
             paginated_data = paginate(likers_response, tweet_id, "likes")
-            likers_data.append(paginated_data)
+            for data in paginated_data:
+                likers_data.append(data)
     except:
         print("likers_response did not need to be paginated")
 
     try:
         if retweeters_response.meta['next_token']:
             paginated_data = paginate(retweeters_response, tweet_id, "retweets")
-            retweeters_data.append(paginated_data)
+            for data in paginated_data:
+                retweeters_data.append(data)
     except:
         print("retweeters_response list did not need to be paginated")
 
@@ -114,34 +129,91 @@ for tweet_id in giveaway_thread:
             
 # create new list of users who liked AND retweeted the tweets
 liked_retweeted = [username for username in likers_list if username in retweeters_list]
-print("---- Total Accounts Liked+Retweeted Giveaway Tweets ----")
-print(len(liked_retweeted))
-print(liked_retweeted)
+#print("---- Total Accounts Liked+Retweeted Giveaway Tweets ----")
+#print(len(liked_retweeted))
+#print(liked_retweeted)
 
+almost_qualified = [username for username in liked_retweeted if username in following_accounts]
+
+users_to_request = []
+users_list = []
+users_count = 0
+total_user_count = 0
+qualified_count = len(almost_qualified)
+for user in almost_qualified:
+    if users_count < 100:
+        users_list.append(user)
+        users_count += 1
+        total_user_count += 1
+    else:
+        users_to_request.append(users_list)
+        users_list = []
+        users_count = 0
+        users_list.append(user)
+        users_count += 1
+        total_user_count += 1
+    
+    if total_user_count == qualified_count:
+        users_to_request.append(users_list)
+
+#print(users_to_request)
+#print(len(users_to_request))
+
+print(" --- done with adding to users_to_request --- ")
+#print("--- users_to_request --- ")
+#print(users_to_request)
+user_data = {}
+reversed_user_data = {}
+for user_list in users_to_request:
+    users_response = client.get_users(usernames=user_list)
+    for account in users_response.data:
+        user_data[account.username] = account.id
+        reversed_user_data[account.id] = account.username
+#print(" --- user_data --- ")
+#print(user_data)
 
 # 4. Check for 3 tagged users in reply tweets
-replies = {}
-for tweet_id in giveaway_thread:
-    
-    tweet_replies = client.search_recent_tweets(query="conversation_id:" + str(tweet_id), tweet_fields=['author_id','text'], max_results=100)
-    
-    for tweet in tweet_replies.data:
-        if hasattr(tweet, 'in_reply_to_status_id_str'):
-            if tweet.id not in replies.keys():
-                replies[tweet.id] = [tweet.author_id, tweet.text]
 
+tweet = client.get_tweet(id=1496183164917665793, tweet_fields='created_at')
+giveaway_datetime = tweet.data['created_at']
+replies = {}
+for username in almost_qualified:
+    if username in user_data.keys():
+        user_id = user_data[username]
+        
+        indiv_tweets = client.get_users_tweets(id=user_id, exclude=['retweets', 'replies'], start_time = giveaway_datetime, tweet_fields=['created_at', 'conversation_id', 'author_id', 'text'], max_results=100)
+        no_retweets = client.get_users_tweets(id=user_id, exclude='retweets', start_time = giveaway_datetime, expansions='entities.mentions.username', tweet_fields=['created_at', 'conversation_id', 'author_id', 'text'], max_results=100)
+        
+        indiv_tweets_list = []
+        if isinstance(indiv_tweets.data, Iterable):
+            for tweet in indiv_tweets.data:
+                indiv_tweets_list.append(tweet.id)
+
+        #sort tweets to get just the user reply tweets on the giveaway thread
+        if isinstance(no_retweets.data, Iterable):
+            #print("reading retweets")
+            for tweet in no_retweets.data:
+                #print("reading individual tweets")
+                if tweet.id not in indiv_tweets_list and tweet.conversation_id in giveaway_thread:
+                    if user_id not in replies.keys():
+                        replies[user_id] = tweet.text
+#print(replies)
 tagged_3 = []
-for reply_id in replies.keys():
-    tweet_text_list = list(replies[reply_id][1])
-    count = 0
-    for word in tweet_text_list:
-        if '@' in word:
-            count += 1
-    if count >= 3:
-        tagged_3.append(replies[reply_id][0])
+for user_id in replies.keys():
+    mention_count = 0
+    tweet_text = list(replies[user_id])
+    for text in tweet_text:
+        if '@' in text:
+            mention_count += 1
+    if mention_count >= 3:
+        if user_id in user_data.values():
+            tagged_3.append(reversed_user_data[user_id])
+#print(tagged_3)
 
 # Aggregate total users who qualify for giveaway
-qualified_users = [username for username in following_accounts if (username in liked_retweeted and username in tagged_3)]
+qualified_users = [username for username in almost_qualified if username in tagged_3]
 
-print(qualified_users)
-
+#print(" -- almost qualified -- ")
+#print(almost_qualified)
+#print(" -- qualified users -- ")
+#print(qualified_users)
